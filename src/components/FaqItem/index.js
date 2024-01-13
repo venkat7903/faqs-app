fb542b8ff58761d833fe756bc4038d6b9949a971
raja <rajababu095221@gmail.com>
2024-01-13 17:19:23 +0000

    first commit

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,30 +1,37 @@
+import {Component} from 'react'
 import './index.css'
 
-function FaqItem(props) {
-  const {faqItemDetails, togglePlusMinus} = props
-  const {id, questionText, answerText, isAnswerVisible} = faqItemDetails
-  const imgUrl = isAnswerVisible
-    ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
-    : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
-  const altText = isAnswerVisible ? 'minus' : 'plus'
-  function onClickPlusMinus() {
-    togglePlusMinus(id)
+class FaqItem extends Component {
+  state = {isActive: false}
+
+  onToggleBtn = () => {
+    this.setState(prevState => ({isActive: !prevState.isActive}))
   }
-  return (
-    <li className="faq-item">
-      <div className="ques-icon-container">
-        <h1 className="question-text">{questionText}</h1>
-        <button
-          type="button"
-          className="plus-minus-btn"
-          onClick={onClickPlusMinus}
-        >
-          <img src={imgUrl} alt={altText} className="plus-minus-img" />
-        </button>
-      </div>
-      {isAnswerVisible && <p className="answer-text">{answerText}</p>}
-    </li>
-  )
+
+  render() {
+    const {isActive} = this.state
+    const {faqItemDetails} = this.props
+    const {questionText, answerText} = faqItemDetails
+    const imgUrl = isActive
+      ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+    const altText = isActive ? 'minus' : 'plus'
+    return (
+      <li className="faq-item">
+        <div className="ques-icon-container">
+          <h1 className="question-text">{questionText}</h1>
+          <button
+            type="button"
+            className="plus-minus-btn"
+            onClick={this.onToggleBtn}
+          >
+            <img src={imgUrl} alt={altText} className="plus-minus-img" />
+          </button>
+        </div>
+        {isActive && <p className="answer-text">{answerText}</p>}
+      </li>
+    )
+  }
 }
 
 export default FaqItem
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,50 +1,21 @@
 // Write your code here.
-import {Component} from 'react'
 import FaqItem from '../FaqItem'
 import './index.css'
 
-class Faqs extends Component {
-  state = {faqsList: []}
-
-  componentDidMount() {
-    const {faqsList} = this.props
-    const modifiedFaqsList = faqsList.map(each => ({
-      ...each,
-      isAnswerVisible: false,
-    }))
-    this.setState({faqsList: modifiedFaqsList})
-  }
-
-  togglePlusMinus = id => {
-    this.setState(prevState => ({
-      faqsList: prevState.faqsList.map(each => {
-        if (each.id === id) {
-          return {...each, isAnswerVisible: !each.isAnswerVisible}
-        }
-        return each
-      }),
-    }))
-  }
-
-  render() {
-    const {faqsList} = this.state
-    return (
-      <div className="faqs-app-container">
-        <div className="sub-faqs-app-container">
-          <h1 className="faqs-title">FAQs</h1>
-          <ul className="faqs-list">
-            {faqsList.map(each => (
-              <FaqItem
-                key={each.id}
-                faqItemDetails={each}
-                togglePlusMinus={this.togglePlusMinus}
-              />
-            ))}
-          </ul>
-        </div>
+function Faqs(props) {
+  const {faqsList} = props
+  return (
+    <div className="faqs-app-container">
+      <div className="sub-faqs-app-container">
+        <h1 className="faqs-title">FAQs</h1>
+        <ul className="faqs-list">
+          {faqsList.map(each => (
+            <FaqItem key={each.id} faqItemDetails={each} />
+          ))}
+        </ul>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Faqs
